269230013ccf64d4d569a9b80b0eaddf670200ea
Dmitriy Synkov <dmitriysynkov@gmail.com>
2017-11-04 02:08:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,163 @@
+
+# Twitter List Manager
+
+This module provides a set of functions build on top of [`tweepy`](https://github.com/tweepy/tweepy) for users to scrape, analyze, export, and copy publically available list on Twitter - or alternatively, create and manage their own. It may be useful for those looking to get lists in or out of Twitter quickly in bulk for use in other kinds of analayses (digitial analytics or social listening) without necessarily requiring the larger extent of `tweepy`'s capabilities. More updates and features to come. 
+
+## Getting Authenticated
+
+***(There are many ways to set up authentication - this is just one approach that consistently works for me.)***
+
+Create an app using your Twitter account and store the following variables in the `source.sh` template file, which you'll use to export your keys and tokens as environment variables. 
+
+
+```python
+$ export CONSUMER_KEY='YOUR CONSUMER_KEY HERE'
+$ export CONSUMER_SECRET='YOUR CONSUMER_KEY HERE'
+$ export ACCESS_TOKEN='YOUR ACCESS_TOKEN HERE'
+$ export ACCESS_TOKEN_SECRET='YOUR ACCESS_TOKEN_SECRET HERE'
+$ export OWNER='YOUR OWNDER NAME HERE'
+$ export OWNER_ID='YOUR OWNER_ID HERE'
+```
+
+In your terminal, run the following command:
+
+
+```python
+$ source source.sh
+```
+
+To check that your environment variables have been stored, run the below command using any variable. I'll use `CONSUMER_KEY` in this example. If successful, you should see a print-out of your key.
+
+
+```python
+$ echo $CONSUMER_KEY
+```
+
+To check these are being picked up by Python run the below commands in your Python console. You should see the same output as above.
+
+
+```python
+import os
+
+print(os.environ.get('CONSUMER KEY'))
+```
+
+***(For Windows user, look up how to replicate the above steps using `setx` instead of `export` and a `.bat` instead of a `.sh` file. Or, just get the Bash emulator that comes with the installation of [Git for Windows](https://git-for-windows.github.io/).)***
+
+Once your keys are squared away, you should be able to authenticate immediately after creating an instance of the `TwitterListManager()` class:
+
+
+```python
+from twitter_list_manager import TwitterListManager
+
+t = TwitterListManager()
+```
+
+Now, post a tweet to verify authentication:
+
+
+```python
+t.post_tweet("Let's check out what we can do with Twitter lists!")
+```
+
+    Success! You tweeted 'Let's check out what we can do with Twitter lists!' on 2017-11-04 01:26:56.
+    
+
+## Working With Public Lists
+
+To get extract the list members from a publically available Twitter list, use the `.get_list_members()` method with the two required parameters of `owner` (the list owner's handle, sans '@', in quotes) and the list `slug`, also in quotes. We'll use appropriately titled *Foreign Policy's* [Twitterati 100](https://twitter.com/foreignpolicy/lists/twitterati-100?lang=en). 
+
+You should be able to find the slug in the tail-end portion of the URL, right before `lang` if `lang` is present. So, for a URL of `https://twitter.com/foreignpolicy/lists/twitterati-100?lang=en` the slug would be `twitterati-100`. This will return a `list` type object of user screen names.
+
+
+```python
+t.get_list_members('foreignpolicy','twitterati-100')
+```
+
+If you preffer user IDs instead of screen names, set `attr=` ("attribute") to `'id'`
+
+
+```python
+t.get_list_members('foreignpolicy','twitterati-100',attr='id')
+```
+
+To get list data -- or the entirety of the `tweepy` `List` object, use the `.get_list_member_data()` method. Set `export=` to `True` if you want it the data exported to `.csv`. The new file will be saved in the `exports` sub-directory of this repository. (`Export` is by default set to `False`.) This method will return a `pandas` dataframe.
+
+
+```python
+t.get_list_member_data('foreignpolicy','twitterati-100',export=True)
+```
+
+Now, to get a *list of lists* from a particular owner (user), simply pass the owner's handle (again, in quotes, and with no '@') to the `.get_owner_lists()` method, returning a `list` object.
+
+
+```python
+t.get_owner_lists('foreignpolicy')
+```
+
+Use `.get_owner_list_data()` for a more detailed look at a user's lists, including the full list name, description, count of subscribers, count of members, and the created time. Again, setting `export=` to `True` will write a `.csv` to your local directory. This method returns a dataframe.
+
+
+```python
+t.get_owner_list_data('foreignpolicy',export=True)
+```
+
+## Working With Your Own Lists
+
+This next set of fucntions are parallel to the above, only you don't need to specy the `owner` parameter, since the owner is you (assuming you exported `OWNER` as an environment variable):
+
+
+```python
+t.get_my_lists() # Returns a list of your Twitter lists
+```
+
+
+```python
+t.get_my_list_data(export=True) # Returns a dataframe of your list data
+```
+
+To create a list, you need to pass a name and either:
+
+1. A Python list of screen names or Twitter ids you want added.
+2. A text file of screen names or Twitter ids you want added.
+
+Note that if you're sourcing from a text file, the items must be in a single column, with no '@'s or commas.
+
+**For *both* cases (1) & (2) above, the source has must be *all* screen names or *all* Twitter IDs.**
+
+See the text files in the `examples` sub-directory of this repository for both cases.
+
+Say we saved the *Foreign Policy* list from earlir into a list object and want to create a list from it outselves:
+
+
+```python
+twitterati = t.get_list_members('foreignpolicy','twitterati-100')
+```
+
+We can then pass it to the `.create_list()` method, preceded by a name.
+
+
+```python
+t.create_list('Twitterati Copy List',twitterati)
+```
+
+Alternatively, we can source our list from a text file, using `list_as_text_file_ids.txt` as an example. Make sure you include an *absolute* (rather than a relative) filepath.
+
+
+```python
+filepath = '/c/path/to/twitter_list_manager/examples/list_as_text_file_ids.txt'
+
+t.create_list('Twitterati Copy List',filepath)
+```
+
+You can also copy a publically available Twitter list to your own account:
+
+
+```python
+t.copy_list('foreignpolicy','twitterati-100')
+```
+
+    List 'Twitterati 100' with slug 'twitterati-100' copied from @foreignpolicy on 2017-11-04 01:28:40.
+    
+
+Lastly, to delete **all** of your lists, use the `.delete_all_lists()`. Please note this will delete **all** Twitter lists currently in your account. You you need to remove a single list, refer to the `API.destroy_list()` method in the [`tweepy` documentation](http://docs.tweepy.org/en/v3.5.0/api.html). 
---
+++ twitter_list_manager.py
@@ -0,0 +1,240 @@
+from datetime import datetime
+import pandas as pd
+import tweepy 
+import json
+import time
+import csv
+import os
+
+auth_dict = {
+    'CONSUMER_KEY': os.environ.get('CONSUMER_KEY'),
+    'CONSUMER_SECRET': os.environ.get('CONSUMER_SECRET'),
+    'ACCESS_TOKEN': os.environ.get('ACCESS_TOKEN'),
+    'ACCESS_TOKEN_SECRET': os.environ.get('ACCESS_TOKEN_SECRET'),
+    'OWNER': os.environ.get('OWNER'),
+    'OWNER_ID': os.environ.get('OWNER_ID')
+}
+
+path_error_msg = "The argument 'members = ' must contain either a list object or an absolute path to a file containing list of members."
+
+class TwitterListManager:
+    
+    def __init__(self):
+        
+        auth = tweepy.OAuthHandler(
+            auth_dict['CONSUMER_KEY'],
+            auth_dict['CONSUMER_SECRET'])
+            
+        auth.set_access_token(
+            auth_dict['ACCESS_TOKEN'],
+            auth_dict['ACCESS_TOKEN_SECRET'])
+        
+        global OWNER 
+        OWNER = auth_dict['OWNER']
+        
+        global OWNER_ID 
+        OWNER_ID = auth_dict['OWNER_ID']
+        
+        global API
+        API = tweepy.API(auth)
+    
+    def post_tweet(self,text):
+
+        API.update_status(status=text)
+        
+        current_time = time.time()
+        current_time_stamp = datetime.fromtimestamp(
+            current_time).strftime('%Y-%m-%d %H:%M:%S')
+            
+        print("Success! You tweeted '{}' on {}.".format(text,current_time_stamp))
+
+    def get_list_members(self,owner,slug,attr='screen_name'):
+    
+        output_list = []
+        
+        for member in tweepy.Cursor(API.list_members,owner,slug).items():
+            if attr == 'id':
+                output_list.append(member.id)
+            else:
+                output_list.append(member.screen_name)
+        
+        return output_list
+
+    def get_list_member_data(self,owner,slug,export=False):
+        
+        # Initiate empty df
+        member_df = pd.DataFrame()
+        
+        for member in tweepy.Cursor(API.list_members,owner,slug).items():
+            # Create json string from User object
+            member_json_str = json.dumps(member._json)
+            # Read json into line as pandas df
+            member_df_row = pd.read_json(member_json_str,lines=True)
+            # Append each line into member_df
+            member_df = member_df.append(member_df_row)
+            # To do: write output to csv instead of a df
+        if export:
+            filepath = os.getcwd() + '\\exports\\'
+            filename = owner + '-' + slug + '-list.csv'
+            member_df.to_csv(filepath + filename, index=False)
+            
+        return member_df
+
+    def get_owner_lists(self,owner):
+        list_result_set = API.lists_all(owner)
+        list_names = []
+        for item in list_result_set:
+            list_names.append(item.name)
+        return list_names
+
+    def get_owner_list_data(self,owner,export=False):
+        list_result_set = API.lists_all(owner)
+        
+        # Create fields
+        list_names = []
+        full_name = []
+        description = []
+        subscriber_count = []
+        members_count = []
+        created_at = []
+        
+        # To do: write to csv instead of a df
+        
+        for item in list_result_set:
+            list_names.append(item.name)
+            full_name.append(item.full_name)
+            description.append(item.description)
+            subscriber_count.append(item.subscriber_count)
+            members_count.append(item.member_count)
+            created_at.append(item.created_at)
+        list_df = pd.DataFrame({
+            'list_name': list_names,
+            'full_name': full_name,
+            'description': description,
+            'subscriber_count': subscriber_count,
+            'members_count': members_count,
+            'created_at': created_at
+        })    
+        
+        if export:
+            filepath = os.getcwd() + '\\exports\\'
+            filename = owner + '-data.csv'
+            list_df.to_csv(filepath + filename, index=False)
+        
+        return list_df
+
+    def get_my_lists(self):
+        list_result_set = API.lists_all(OWNER)
+        list_names = []
+        for item in list_result_set:
+            list_names.append(item.name)
+            
+        return list_names
+
+    def get_my_list_data(self,export=False):
+        list_result_set = API.lists_all(OWNER)
+        
+        # Create fields
+        list_names = []
+        full_name = []
+        description = []
+        subscriber_count = []
+        members_count = []
+        created_at = []
+        
+        # To do: write to csv instead of a df
+        # To do: option to return as List object
+        for item in list_result_set:
+            list_names.append(item.name)
+            full_name.append(item.full_name)
+            description.append(item.description)
+            subscriber_count.append(item.subscriber_count)
+            members_count.append(item.member_count)
+            created_at.append(item.created_at)
+        list_df = pd.DataFrame({
+            'list_name': list_names,
+            'full_name': full_name,
+            'description': description,
+            'subscriber_count': subscriber_count,
+            'members_count': members_count,
+            'created_at': created_at
+        })    
+        
+        if export:
+            filepath = os.getcwd() + '\\exports\\'
+            filename = OWNER + '-list-data.csv'
+            list_df.to_csv(filepath + filename, index=False)
+            
+        return list_df
+
+    def create_list(self,name,members):
+
+        # To do: add error handling
+        # for non-existent handles/ids
+        if os.path.isabs(str(members)):
+            members_file = open(members,'r')
+            members = members_file.read().splitlines()
+            
+        if isinstance(members,list):
+            # Check if list items are int or str
+            # To do: turn this into a function
+            check_first_member = isinstance(members[0],str)
+            
+            # If str then retrieve the user ids
+            # To do: turn this into a function
+            if check_first_member:
+                member_ids = []
+                for member in members:
+                    user = API.get_user(member)
+                    member_ids.append(user.id)
+            else:
+                member_ids = members
+            
+            # Create the actual list; get slug
+            new_list = API.create_list(name)
+            new_slug = new_list.slug
+            
+            for member_id in member_ids:
+                API.add_list_member(user_id = member_id, slug= new_slug, 
+                                    owner_screen_name = OWNER)
+        else:
+            raise TypeError(path_error_msg)
+
+    def copy_list(self,owner,slug):
+    
+        # To do: error handling for rate limits for large lists
+    
+        # Get the actual list object
+        list_source = API.get_list(owner_screen_name=owner,slug=slug)
+        
+        # Get the list name to copy 
+        list_name = list_source.name
+        
+        # Get members from the list
+        list_to_copy = self.get_list_members(owner,slug)  
+        
+        # Create copy using same name
+        self.create_list(list_name,list_to_copy)
+        
+        current_time = time.time()
+        current_time_stamp = datetime.fromtimestamp(
+            current_time).strftime('%Y-%m-%d %H:%M:%S')
+        
+        print("List '{}' with slug '{}' copied from @{} on {}.".format(
+            list_name,slug,owner,current_time_stamp))
+
+    def delete_all_lists(self):
+    
+        list_result_set = API.lists_all(OWNER)
+        list_slugs = []
+        
+        if len(list_result_set) > 0:
+            for item in list_result_set:
+                list_slugs.append(item.slug)
+            for slug in list_slugs:
+                API.destroy_list(owner_screen_name=OWNER,slug=slug)
+                
+            print("All {} of your lists have been deleted.".format(len(list_slugs)))
+            
+        else:
+            print("You don't have any lists to delete.")
